4f21f8767f4a93ffb35192cf7eabaa3249f3d8a4
VlaDinter <vladyslavdinter@gmail.com>
2020-02-10 16:33:27 +0200

    write tests

---
+++ src/components/app/app.test.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App from './app.jsx';
+
+it(`App correctly renders after relaunch`, () => {
+  const tree = renderer.create(<App
+    gameTime={0}
+    errorCount={0}
+  />).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import renderer from 'react-test-renderer';
+
+import WelcomeScreen from './welcome-screen.jsx';
+
+it(`WelcomeScreen correctly renders after relaunch`, () => {
+  const tree = renderer.create(<WelcomeScreen
+    time={0}
+    errorCount={0}
+  />).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
