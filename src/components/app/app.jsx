09e13405d5160148aa92f203cad240fa0c70bdaa
VlaDinter <vladyslavdinter@gmail.com>
2020-02-12 03:45:14 +0200

    create question screens

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,24 +1,75 @@
-﻿import React from 'react';
+﻿import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen.jsx';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen.jsx';
 
-const App = (props) => {
-  const {
-    gameTime,
-    errorCount,
-  } = props;
+class App extends PureComponent {
+  static getScreen(question, props, onUserAnswer) {
+    if (question === -1) {
+      const {
+        gameTime,
+        errorCount,
+      } = props;
 
-  return <WelcomeScreen
-    time={gameTime}
-    errorCount={errorCount}
-    onClick={() => {}}
-  />;
-};
+      return <WelcomeScreen
+        time={gameTime}
+        errorCount={errorCount}
+        onStartButtonClick={onUserAnswer}
+      />;
+    }
+
+    const {questions} = props;
+    const currentQuestion = questions[question];
+
+    switch (currentQuestion.type) {
+      case `genre`: return <GenreQuestionScreen
+        genre={currentQuestion.genre}
+        answers={currentQuestion.answers}
+        onAnswer={onUserAnswer}
+      />;
+
+      case `artist`: return <ArtistQuestionScreen
+        song={currentQuestion.song}
+        answers={currentQuestion.answers}
+        onAnswer={onUserAnswer}
+      />;
+    }
+
+    return null;
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      question: -1,
+    };
+  }
+
+  render() {
+    const {questions} = this.props;
+
+    const {question} = this.state;
+
+    return App.getScreen(question, this.props, () => {
+      this.setState((prevState) => {
+        const nextIndex = prevState.question + 1;
+        const isEnd = nextIndex >= questions.length;
+
+        return {
+          question: !isEnd ? nextIndex : -1,
+        };
+      });
+    });
+  }
+}
 
 App.propTypes = {
   gameTime: PropTypes.number.isRequired,
   errorCount: PropTypes.number.isRequired,
+  questions: PropTypes.array.isRequired,
 };
 
 export default App;
--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -3,11 +3,62 @@ import renderer from 'react-test-renderer';
 
 import App from './app.jsx';
 
+const mock = [
+  {
+    type: `genre`,
+    genre: `country`,
+    answers: [
+      {
+        src: `test.mp3`,
+        genre: `country`,
+      },
+      {
+        src: `test.mp3`,
+        genre: `electronic`,
+      },
+      {
+        src: `test.mp3`,
+        genre: `reggae`,
+      },
+      {
+        src: `test.mp3`,
+        genre: `alternative`,
+      },
+    ],
+  },
+  {
+    type: `artist`,
+    song: {
+      artist: `Unicorn Heads`,
+      src: `path.mp3`,
+    },
+    answers: [
+      {
+        picture: `path.jpg`,
+        artist: `Spazz Cardigan`,
+      },
+      {
+        picture: `path.jpg`,
+        artist: `Density & Time`,
+      },
+      {
+        picture: `path.jpg`,
+        artist: `Unicorn Heads`,
+      },
+    ],
+  },
+];
+
 it(`App correctly renders after relaunch`, () => {
   const tree = renderer.create(<App
     gameTime={0}
     errorCount={0}
-  />).toJSON();
+    questions={mock}
+  />, {
+    createNodeMock: () => {
+      return {};
+    }
+  }).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
